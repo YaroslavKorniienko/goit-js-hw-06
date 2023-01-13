61e5515028e776b4c89857b4348c5e99f403d9b2
YaroslavKorniienko <yarikkornej@gmail.com>
2023-01-13 17:25:57 +0200

    done task 03

---
+++ js/task-02.js
@@ -0,0 +1,17 @@
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+
+const ingredientElem = document.getElementById("ingredients");
+for (const ingredient of ingredients) {
+  const liElem = document.createElement("li");
+  liElem.textContent = ingredient;
+  liElem.classList.add("item");
+  ingredientElem.appendChildd(liElem);
+}
+// ingredientElem.appendChild(ingredientElem);
